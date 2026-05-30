489c291679e71d50e0b84c4d366569d624e29b07
yuusuke <xiangzhongyoufu@gmail.com>
2026-05-30 21:01:46 +0900

    refactor: 関数名をわかりやすくする

--- backend/tests/test_delete_todo.py
+++ backend/tests/test_delete_todo.py
@@ -2,7 +2,7 @@ from app.models.users import User
 from app.extentions import db
 from app.extentions import bcrypt
 
-def test_update_todo(client):
+def test_delete_todo(client):
     password = "password"
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
     
