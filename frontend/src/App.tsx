d21952adc849044d6675592af4ea3c23c4b93d00
yuusuke <xiangzhongyoufu@gmail.com>
2026-05-31 22:41:29 +0900

    add: トーストが最大3つまで表示されるようにする

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SnackbarProvider } from "notistack";
 import SignupForm from './features/signup';
 import LoginForm from './features/login';
 import NotFound from './utils/notfound';
@@ -7,15 +8,17 @@ import Home from './home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/signup' element={<SignupForm />} />
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Router>
-  )
+    <SnackbarProvider maxSnack={3}>
+      <Router>
+        <Routes>
+          <Route path='/signup' element={<SignupForm />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
+    </SnackbarProvider>
+ )
 }
 
 export default App
