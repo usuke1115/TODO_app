93b818b9b5e4d65637fd3238aab4f87b0dc2cb19
yuusuke <xiangzhongyoufu@gmail.com>
2026-06-17 21:20:43 +0900

    update: run.py を直接起動した時のみサーバを起動させる

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,5 @@
 from app import create_app
 
-app = create_app()
-app.run(debug=True, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=8080)
