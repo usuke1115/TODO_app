a3003eb795434cbc7e9fb439f4dbbb0a3d071397
yuusuke <xiangzhongyoufu@gmail.com>
2026-06-21 14:50:21 +0900

    init: ステップバーを追加し、登録手続きをわかりやすくする

---
+++ frontend/src/features/signup/components/stepper.tsx
@@ -0,0 +1,18 @@
+export const Stepper = () => {
+    return (
+        <div className="stepper">
+            <div className="step">
+                <div className="circle">1</div>
+                <div>Step 1</div>
+            </div>
+            <div className="step">
+                <div className="circle">2</div>
+                <div>Step 2</div>
+            </div>
+            <div className="step">
+                <div className="circle">3</div>
+                <div>Step3</div>
+            </div>
+        </div>
+    )
+};
